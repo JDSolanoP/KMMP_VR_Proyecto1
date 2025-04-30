7799f26933174537ee4f83fbd1e50e30eb980c43
JDSolanoP <95657135+JDSolanoP@users.noreply.github.com>
2025-04-30 16:53:02 -0500

    inspeccion930E5 ver. 0.9.7.30.4.25
    revision de sistema de panelesy colision que permitan acceso internos , y paneles en ingles

--- prueba last version/Assets/SCRIPTS/InspeccionCamion/TM_INSPECCIONCAMION.cs
+++ prueba last version/Assets/SCRIPTS/InspeccionCamion/TM_INSPECCIONCAMION.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class TM_INSPECCIONCAMION : Lista_Tareas_Controller
 {
+    public int auxContacto;
     [Header("Elementos")]
     public GameObject[] Muros;
-    public GameObject[] Fechas;
+    public GameObject[] Flechas;
+    public GameObject[] BtnContinue;
     public override void Start()
     {
         base.Start();
@@ -26,7 +28,16 @@ public class TM_INSPECCIONCAMION : Lista_Tareas_Controller
                 {
                     Tablero_Indicaciones[i].SetActive(false);
                 }
-                Tablero_Indicaciones[0].SetActive(true);//panel de conformidad
+                for (int i = 0; i < Flechas.Length; i++)
+                {
+                    Flechas[i].SetActive(false);
+                    aros_indicadores[i].SetActive(false);
+                }
+                for (int i = 0; i < BtnContinue.Length; i++)
+                {
+                    BtnContinue[i].SetActive(false);
+                }
+                Tablero_Indicaciones[0].SetActive(true);//panel de bienvenido
                 /*   //audioManager de bienvenida
 
                 yield return new WaitForSeconds(0.5f);
@@ -46,12 +57,205 @@ public class TM_INSPECCIONCAMION : Lista_Tareas_Controller
 
                     yield return new WaitForFixedUpdate();
                 }
-                yield return new WaitForSecondsRealtime(23f);
-                //Locucion para panel intro de P1
-                
+                yield return new WaitForSecondsRealtime(10f);
+                Tablero_Indicaciones[1].SetActive(true);//Locucion para panel intro de P1
+                Flechas[0].SetActive(true);
+                aros_indicadores[0].SetActive(true);
                 break;
-            
+            case 1://ARO_00
+                auxContacto++;
+                Tablero_Indicaciones[TareaActual+1].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual-1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 2://ARO_01
+                auxContacto++;
+                Tablero_Indicaciones[TareaActual + 1].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 3://ARO_02
+                auxContacto++;
+                Debug.Log(TareaActual + "Nuevo AuxContacto aumentado en Tarea==" + auxContacto);
+                Tablero_Indicaciones[TareaActual + 1].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 4://ARO_03
+                auxContacto++;
+                Debug.Log(TareaActual + "Nuevo AuxContacto aumentado en Tarea==" + auxContacto);
+                Tablero_Indicaciones[TareaActual + 1].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 5://ARO_04
+                auxContacto++;
+                Debug.Log(TareaActual + "Nuevo AuxContacto aumentado en Tarea==" + auxContacto);
+                Tablero_Indicaciones[TareaActual + 2].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 6://ARO_05
+                auxContacto++;
+                Debug.Log(TareaActual + "Nuevo AuxContacto aumentado en Tarea==" + auxContacto);
+                Tablero_Indicaciones[TareaActual + 2].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 7://ARO_06
+                auxContacto++;
+                Debug.Log(TareaActual+" Nuevo AuxContacto aumentado en Tarea==" + auxContacto);
+                Tablero_Indicaciones[TareaActual + 2].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 8://CONTINUAR DE CABINA
+                auxContacto++;
+                Debug.Log(TareaActual + " Nuevo AuxContacto aumentado en Tarea==" + auxContacto);
+                Tablero_Indicaciones[TareaActual + 2].SetActive(true);//PANEL DE VICTORIA
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 9:////ARO_07
+                Muros[8].SetActive(true);
+                aros_indicadores[8].SetActive(false);
+                auxContacto++;
+                Debug.Log(TareaActual + " Nuevo AuxContacto aumentado en Tarea==" + auxContacto);
+                Tablero_Indicaciones[TareaActual + 2].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                break;
+            case 10://CONTINUAR DE PANEL DE CONCLUSIONES
+                Tablero_Indicaciones[12].SetActive(true);//Locucion para panel intro de P1
+                Muros[8].SetActive(false);
+                auxContacto++;
+                Debug.Log(TareaActual + " Nuevo AuxContacto aumentado en Tarea==" + auxContacto);
+                Tablero_Indicaciones[TareaActual + 2].SetActive(true);//Locucion para panel intro de P1
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual - 1].SetActive(true);
+                while (AudioManager.aSource.IsPlayingVoz() == true)
+                {
+
+                    yield return new WaitForFixedUpdate();
+                }
+                yield return new WaitForSecondsRealtime(5);
+                BtnContinue[TareaActual].SetActive(true);
+                break;
         }
     }
     //**********************VERIFICAR CONTACTO OBJETO A OBJETO****************************************
+    public void verificarContacto(int confirmarcontacto)
+    {
+        switch (confirmarcontacto)
+        {
+            case 0://contacto con aros, completa tareas segun aux
+                aSource.goFx("Bien");
+                Debug.Log("auxcontacto=" + auxContacto);
+                if (auxContacto < Flechas.Length)
+                {
+                    Flechas[auxContacto].SetActive(false);
+                }
+                if (auxContacto <aros_indicadores.Length)
+                {
+                    aros_indicadores[auxContacto].SetActive(false);
+                }
+                TareaCompletada(auxContacto);
+                break;
+        }
+    }
+    public void activacionXR(int contacto) //contacto con manos
+    {
+        switch (contacto) 
+        {
+            case 0://boton continue
+                Debug.Log("auxcontacto=" + auxContacto);
+                if (auxContacto < 8) {
+                    Muros[auxContacto - 1].SetActive(false);
+                    aros_indicadores[auxContacto].SetActive(true);
+                    Flechas[auxContacto].SetActive(true);
+                }
+                
+                if (TareaActual < 4)//antes de subir a cabina
+                {
+                    Tablero_Indicaciones[auxContacto + 1].SetActive(false);
+                    if (TareaActual==2)
+                    {
+                        Tablero_Indicaciones[1].SetActive(false);
+                        Tablero_Indicaciones[0].SetActive(false);
+                    }
+                }
+                else if(TareaActual==4)
+                {
+                    Tablero_Indicaciones[auxContacto + 1].SetActive(false);
+                    Tablero_Indicaciones[auxContacto + 2].SetActive(true);
+                }
+                else
+                {
+                    Tablero_Indicaciones[auxContacto + 2].SetActive(false);
+                }
+                if (auxContacto == 7)
+                {
+                    Muros[7].SetActive(false);
+                    Tablero_Indicaciones[6].SetActive(false);
+                    TareaCompletada(TareaActual);
+                }
+                if (auxContacto == 9)
+                {
+                    Muros[7].SetActive(false);
+                    Tablero_Indicaciones[6].SetActive(false);
+                    TareaCompletada(TareaActual);
+                }
+                break;
+        }
+    }
+    public IEnumerator ActiveContinue(GameObject obj)
+    {
+        yield return new WaitForSecondsRealtime(5);
+        obj.SetActive(true);
+    }
 }
